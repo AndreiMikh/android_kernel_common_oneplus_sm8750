d99f158d7772695477952b01f1ea0c8bf5fe431a
AndreiMikh <andreimikhail0087@gmail.com>
2026-04-07 14:26:18 +0800

    Update core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -96,6 +96,8 @@
 #include "../../io_uring/io-wq.h"
 #include "../smpboot.h"
 
+#include <linux/sched/bore.h>
+
 #ifdef CONFIG_HMBIRD_SCHED
 #include "hmbird/slim.h"
 #include "hmbird/hmbird_shadow_tick.h"
@@ -10366,6 +10368,10 @@ void __init sched_init(void)
 #endif
 #endif
 
+#ifdef CONFIG_SCHED_BORE
+	sched_bore_init();
+#endif // CONFIG_SCHED_BORE
+	
 	wait_bit_init();
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
